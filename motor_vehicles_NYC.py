81d2edee8f553e5a28fa843d33ac84f55eee4f20
Spartan1203 <54310316+Spartan1203@users.noreply.github.com>
2023-03-07 02:00:52 -0500

    Update motor_vehicles_NYC.py

--- motor_vehicles_NYC.py
+++ motor_vehicles_NYC.py
@@ -20,7 +20,7 @@ st.markdown("This is a streamlit dashboard to monitor vehicle collision in NYC")
 
 @st.cache(persist=True)
 def load_data(nrows):
-	data=pd.read_csv(DATA_URL,nrows=nrows, header=False)
+	data=pd.read_csv(DATA_URL)
 	return data
 data = load_data(100000)
 data.dropna(subset=['LATITUDE','LONGITUDE'],inplace=True)
